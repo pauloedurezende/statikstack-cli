c706379fdaa9501a6e070032b0bbde399e6b45e8
Paulo Rezende <rezendepauloedu@outlook.com>
2018-09-04 00:43:10 -0300

    add main CLI logic

---
+++ src/index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+const { resolve } = require('path');
+const inquirer = require('inquirer');
+const simpleGit = require('simple-git');
+
+const repositories = require('./repositories');
+
+const question = {
+  name: 'project-choice',
+  type: 'list',
+  message: 'Which project would you like to generate?',
+  choices: repositories.map((repo) => repo.name)
+};
+
+inquirer.prompt(question).then((answers) => {
+  simpleGit()
+    .exec(() => console.log('Starting the download...'))
+    .clone(
+      `https://github.com/statikstack/${answers['project-choice']}`,
+      resolve(answers['project-choice'])
+    )
+    .exec(() => console.log('Download successful!'));
+});
