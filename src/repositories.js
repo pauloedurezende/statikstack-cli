b22ce1cffb7a93cddc261ff84416662c0930c81e
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2018-09-04 00:49:44 -0300

    add all remaining kits created by the project

--- src/repositories.js
+++ src/repositories.js
@@ -1,8 +1,26 @@
 module.exports = [
   {
+    name: 'html-css-javascript-webpack'
+  },
+  {
+    name: 'html-sass-javascript-webpack'
+  },
+  {
     name: 'html-stylus-javascript-webpack'
   },
   {
+    name: 'html-less-javascript-webpack'
+  },
+  {
+    name: 'html-css-javascript-parcel'
+  },
+  {
     name: 'html-sass-javascript-parcel'
+  },
+  {
+    name: 'html-stylus-javascript-parcel'
+  },
+  {
+    name: 'html-less-javascript-parcel'
   }
 ];
