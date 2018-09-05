22851113799107a862111e549cd97f4096a38cb5
Paulo Rezende <rezendepauloedu@outlook.com>
2018-09-05 10:53:30 -0300

    add file to store the questions

--- src/index.js
+++ src/index.js
@@ -4,21 +4,14 @@ const { resolve } = require('path');
 const inquirer = require('inquirer');
 const simpleGit = require('simple-git');
 
-const repositories = require('./repositories');
+const questions = require('./questions');
 
-const question = {
-  name: 'project-choice',
-  type: 'list',
-  message: 'Which project would you like to generate?',
-  choices: repositories.map((repo) => repo)
-};
-
-inquirer.prompt(question).then((answers) => {
+inquirer.prompt(questions).then(({ selectedRepo }) => {
   simpleGit()
     .exec(() => console.log('Starting the download...'))
     .clone(
-      `https://github.com/statikstack/${answers['project-choice']}`,
-      resolve(answers['project-choice'])
+      `https://github.com/statikstack/${selectedRepo}`,
+      resolve(selectedRepo)
     )
     .exec(() => console.log('Download successful!'));
 });
