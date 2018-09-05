63c99ac58795fd145c1a93bd66ad4e7753e0ff2c
Paulo Rezende <rezendepauloedu@outlook.com>
2018-09-05 11:04:49 -0300

    feat: add question to folder name

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const simpleGit = require('simple-git');
 
 const questions = require('./questions');
 
-inquirer.prompt(questions).then(({ selectedRepo }) => {
+inquirer.prompt(questions).then(({ selectedRepo, folderName }) => {
   simpleGit()
     .exec(() => console.log('Starting the download...'))
     .clone(
       `https://github.com/statikstack/${selectedRepo}`,
-      resolve(selectedRepo)
+      resolve(folderName)
     )
     .exec(() => console.log('Download successful!'));
 });
--- src/questions.js
+++ src/questions.js
@@ -6,5 +6,16 @@ module.exports = [
     type: 'list',
     message: 'Which project would you like to generate?',
     choices: repositories.map((repo) => repo)
+  },
+  {
+    name: 'folderName',
+    type: 'input',
+    message: 'Enter the name of your project',
+    validate(name) {
+      if (name === '') {
+        return 'Please enter a name for the folder';
+      }
+      return true;
+    }
   }
 ];
