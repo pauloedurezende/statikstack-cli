0e301b59405fd45d6e0929d131052760c3630afa
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2018-09-05 11:59:28 -0300

    feat: add process to download dependencies and delete the .git folder

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@
 const { resolve } = require('path');
 const inquirer = require('inquirer');
 const simpleGit = require('simple-git');
+const childProcess = require('child_process');
 
 const questions = require('./questions');
 
 inquirer.prompt(questions).then(({ selectedRepo, folderName }) => {
   simpleGit()
-    .exec(() => console.log('Starting the download...'))
+    .exec(() => console.log('Starting the project download...'))
     .clone(
       `https://github.com/statikstack/${selectedRepo}`,
       resolve(folderName)
     )
-    .exec(() => console.log('Download successful!'));
+    .exec(() => console.log('The Project have been downloaded!'))
+    .exec(() => console.log('Downloading the project dependencies...'))
+    .exec(() =>
+      childProcess.exec(`cd ${folderName} && rm -rf .git && npm install`, () =>
+        console.log('The dependencies have been downloaded!')
+      )
+    );
 });
